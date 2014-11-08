2f3294430967b9bbabde5260273c60ddcb4e72a6
rb <rbhndl@gmail.com>
2014-11-08 17:38:12 +0400

    болванки проектов pomodoro и scrum

---
+++ scrum/main_window.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef MAIN_WINDOW_HPP
+#define MAIN_WINDOW_HPP
+
+#include "ui_main_window.h"
+
+class MainWindow : public QMainWindow, private Ui::MainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    
+protected:
+    void changeEvent(QEvent *e);
+};
+
+#endif // MAIN_WINDOW_HPP
---
+++ scrum/scrum.pro
@@ -0,0 +1,10 @@
+TARGET = scrum
+TEMPLATE = app
+
+QT       += core gui
+
+SOURCES += main.cpp
+
+HEADERS += main_window.hpp
+SOURCES += main_window.cpp
+FORMS   += main_window.ui
